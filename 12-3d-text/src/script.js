2108063b8e64ea59925fa4cc4bde8b8815abf555
gujiaxuan <1615258386@qq.com>
2025-11-14 15:27:54 +0800

    style: text modify

--- 12-3d-text/src/script.js
+++ 12-3d-text/src/script.js
@@ -32,7 +32,7 @@ matcapTexture.colorSpace = THREE.SRGBColorSpace
  */
 const fontLoader = new FontLoader();
 const font = await fontLoader.loadAsync('/fonts/851tegakizatsu_Regular.json')
-const textGeometry = new TextGeometry('我是莫松润的爸爸', {
+const textGeometry = new TextGeometry('我是杨广青 你爸爸', {
     font,
     size: 0.5,
     depth: 0.2,
